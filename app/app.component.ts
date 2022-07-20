45db7dd4d26adb1da7b61da4b8bd693c48822f0a
BisenKaruna <109526135+BisenKaruna@users.noreply.github.com>
2022-07-20 17:21:18 +0530

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'GameCount';
+  oddNums: number[]=[];
+  evenNums: number[]=[];
+  
+  OnincreamentOnEachInterval(increamentedNum:number){
+  //console.log(increamentedNum);
+  
+  if(increamentedNum%2==0){
+  this.evenNums.push(increamentedNum);
+  }
+  else{
+  this.oddNums.push(increamentedNum);
+  }
+    }
+
+}
---
+++ app/game-control/game-control.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Output() increamentOnEachInterval = new EventEmitter<number>(); //outputting this to app component
+   interval:any;
+   previousNum=0;
+ 
+   constructor() { }
+ 
+   ngOnInit() {
+   }
+ // method called on click of start button
+   onStart(){
+ this.interval= setInterval(()=> {
+ this.increamentOnEachInterval.emit(this.previousNum+1);
+ this.previousNum++;
+ },1000)
+   }
+ 
+   onStop(){
+     clearInterval(this.interval);
+   }
+ 
+ }
